e420772c56d8c41c7708acb25bd647d83e47624b
Robert Lamberson <rlamberson@cnm.edu>
2021-11-16 16:00:27 -0600

    Job-listings Static page start

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
+import {JobListings} from "./Job-listings";
 
 export const App = () => (
     <>
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path="/job-listings"component={JobListings}/>
                 <Route component={FourOhFour} />
             </Switch>
         </BrowserRouter>

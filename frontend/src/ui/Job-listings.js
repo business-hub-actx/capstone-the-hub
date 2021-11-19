e33b4c7b3e596c1810d4fb526181c87732f22750
Robert Lamberson <rlamberson@cnm.edu>
2021-11-19 09:41:04 -0600

    added job listings

--- frontend/src/ui/Job-listings.js
+++ frontend/src/ui/Job-listings.js
@@ -1,12 +1,54 @@
 import React from "react";
-import {Container, Image, Row, Col, Card} from "react-bootstrap";
-import jobListingsHeader from "./images/working.jpg"
+import {Container, Row, Col, Form, FormControl} from "react-bootstrap";
 import "./job-listings.css"
+import {ProfileCard} from "./ProfileCard";
 
 export function JobListings() {
     return (
         <>
-            <h1>jajaja</h1>
+            <Container fluid className="proHeader">
+                <Row className="d-flex justify-content-center">
+                    <Col lg={6} sm={6} className="m-5">
+                        <h1 className="proTitle text-center px-2 m-5 border border-2 border-dark">
+                            AMARILLO JOB LISTINGS
+                        </h1>
+                    </Col>
+                </Row>
+                <Row className="d-flex justify-content-center">
+                    <Col lg={3} md={3} sm={4} xs={5} className="mt-lg-5">
+                        <Form className="searchInput m-3">
+                            <FormControl
+                                type="Search"
+                                placeholder="Search"
+                                className="rounded-pill"
+                                aria-label="Search"
+                            />
+                        </Form>
+                    </Col>
+                    <Col lg={3} md={3} sm={4} xs={5} className="mt-lg-5">
+                        <Form.Select className="filterInput m-3">
+                            <option>Filters</option>
+                            <option value="1">Full-Time</option>
+                            <option value="2">Part-Time</option>
+                            <option value="3">Freelance</option>
+                            <option value="4">Remote</option>
+                        </Form.Select>
+                    </Col>
+                </Row>
+            </Container>
+
+            {/*PROFILE LISTINGS*/}
+            <Container className="proList bg-light border border-2">
+                <Col className="proCol d-block align-items-center">
+                    <ProfileCard/>
+                    <ProfileCard/>
+                    <ProfileCard/>
+                    <ProfileCard/>
+                    <ProfileCard/>
+                    <ProfileCard/>
+                    <ProfileCard/>
+                </Col>
+            </Container>
         </>
     )
 }

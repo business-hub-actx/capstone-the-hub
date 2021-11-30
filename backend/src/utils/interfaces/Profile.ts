d602f02e56cc99b1c50a65a07335908c08faf1c2
Chris Pittman <cpittman3@cnm.edu>
2021-11-30 08:00:22 -0600

    Added auth.utils.ts Profile.ts insertProfile.ts selectProfileByProfileActivationToken.ts

---
+++ backend/src/utils/interfaces/Profile.ts
@@ -0,0 +1,13 @@
+export interface Profile{
+    profileId: number | null,
+    profileAboutMe: string | null,
+    profileActivationToken: string | null,
+    profileEmail: string | null,
+    profileHash: string | null,
+    profileJobTitle: string | null,
+    profileName: string | null,
+    profilePhoto: string | null,
+    profileUrl: string | null,
+    profileResume: string,
+    profileSkills: string | null
+}
---
+++ backend/src/utils/profile/insertProfile.ts
@@ -0,0 +1,13 @@
+import {connect} from "../database.utils";
+import {Profile} from "../interfaces/Profile";
+
+export async function insertProfile(profile: Profile) : Promise<string> {
+	try {
+		const mysqlConnection = await connect();
+		const query: string = 'INSERT INTO profile(profileId, profileAboutMe, profileActivationToken, profileEmail, profileHash, profileJobTitle, profileName, profilePhoto, profileUrl, profileResume, profileSkills VALUES UUID_TO_BIN(8de3249c-51dd-4636-a915-a00450452244), :profileAboutMe, :profileActivationToken, :profileEmail, :profileHash, :profileJobTitle, :profileName, :profilePhoto, :profileUrl, :profileResume, :profileSkills';
+		await mysqlConnection.execute(query, profile);
+		return 'Profile Successfully Created'
+	} catch (error) {
+		throw error
+	}
+}

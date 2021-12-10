d36a2707617d71a8682c12103796062b36f86ab6
Bryan Brumley <90947317+Bryan-Brumley@users.noreply.github.com>
2021-12-09 18:04:14 -0600

    corrected files

--- backend/src/utils/post/selectAllPosts.ts
+++ backend/src/utils/post/selectAllPosts.ts
@@ -5,7 +5,7 @@ import {RowDataPacket,} from "mysql2"
 export async function selectAllPosts() : Promise<Post[]> {
     try {
         const mySqlConnection = await connect()
-        const mySqlQuery = 'SELECT BIN_TO_UUID(postId) AS postId, postProfileId, postContactInfo, postEmail, postDescription, postLogo, postCompany, postWebsite FROM post WHERE postid = :postId ORDER BY postCompany ASC'
+        const mySqlQuery = 'SELECT BIN_TO_UUID(postId) AS postId, BIN_TO_UUID(postProfileId) AS postProfileId, postContactInfo, postEmail, postDescription, postLogo, postCompany, postWebsite FROM post ORDER BY postCompany ASC'
         const result = await mySqlConnection.execute(mySqlQuery) as RowDataPacket[]
         return result[0] as Array<Post>
     } catch (error) {

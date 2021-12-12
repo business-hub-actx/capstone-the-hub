9cb959e8d6f3fd7b4ad86ac3cbae8de3e13720f8
Bryan Brumley <90947317+Bryan-Brumley@users.noreply.github.com>
2021-12-12 11:54:47 -0600

    fixed merge conflict

--- frontend/src/ui/Job-listings.js
+++ frontend/src/ui/Job-listings.js
@@ -1,13 +1,13 @@
 import React, {useEffect} from "react";
 import {Container, Row, Col, Form, FormControl} from "react-bootstrap";
 import "./job-listings.css"
-<<<<<<< HEAD
-import {ProfileCard} from "./ProfileCard";
+
+
 import {useDispatch, useSelector} from "react-redux";
 import {fetchAllPosts} from "../store/post";
-=======
+
 import {PostCard} from "./share/components/PostCard";
->>>>>>> 6f754797a6bbdcf68c6d48a9d427e2a63551d522
+
 
 export function JobListings() {
 
@@ -24,12 +24,7 @@ export function JobListings() {
             <Container fluid className="jobHeader">
                 <Row className="d-flex justify-content-center">
                     <Col lg={6} sm={6} className="m-5">
-<<<<<<< HEAD
-
-                        <h1 className="proTitle text-center px-2 m-5 border border-2 border-dark">
-=======
                         <h1 className="jobTitle text-center px-2 m-5 border border-2 border-dark">
->>>>>>> 6f754797a6bbdcf68c6d48a9d427e2a63551d522
                             AMARILLO JOB LISTINGS
                         </h1>
                     </Col>
@@ -57,23 +52,12 @@ export function JobListings() {
                 </Row>
             </Container>
 
-<<<<<<< HEAD
-            {/*PROFILE LISTINGS*/}
-            <Container className="proList bg-light border border-2">
-                <Col className="proCol d-block align-items-center">
-                    {posts.map(post => <PostCard post={post} key={post.postId}/>)}
-=======
+
+
             {/*JOB POST LISTINGS*/}
             <Container className="jobList bg-light border border-2">
                 <Col className="jobCol d-block align-items-center">
-                    <PostCard/>
-                    <PostCard/>
-                    <PostCard/>
-                    <PostCard/>
-                    <PostCard/>
-                    <PostCard/>
-                    <PostCard/>
->>>>>>> 6f754797a6bbdcf68c6d48a9d427e2a63551d522
+                    {posts.map(post => <PostCard post={post} key={post.postId}/>)}
                 </Col>
             </Container>
         </>

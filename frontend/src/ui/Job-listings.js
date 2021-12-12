e7b166521b7646401a6044bd60d82d8afda07949
Bryan Brumley <90947317+Bryan-Brumley@users.noreply.github.com>
2021-12-12 11:44:03 -0600

    redux store for post

--- frontend/src/ui/Job-listings.js
+++ frontend/src/ui/Job-listings.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Container, Row, Col, Form, FormControl} from "react-bootstrap";
 import "./job-listings.css"
 import {ProfileCard} from "./ProfileCard";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchAllPosts} from "../store/post";
 
 export function JobListings() {
+
+    const posts = useSelector( state => state.post ? state.post : []);
+    const dispatch = useDispatch();
+    const effects = () => {
+        dispatch(fetchAllPosts());
+    };
+    const inputs = [];
+    useEffect(effects,inputs);
+
     return (
         <>
             <Container fluid className="proHeader">
                 <Row className="d-flex justify-content-center">
                     <Col lg={6} sm={6} className="m-5">
+
                         <h1 className="proTitle text-center px-2 m-5 border border-2 border-dark">
                             AMARILLO JOB LISTINGS
                         </h1>
@@ -40,13 +52,7 @@ export function JobListings() {
             {/*PROFILE LISTINGS*/}
             <Container className="proList bg-light border border-2">
                 <Col className="proCol d-block align-items-center">
-                    <ProfileCard/>
-                    <ProfileCard/>
-                    <ProfileCard/>
-                    <ProfileCard/>
-                    <ProfileCard/>
-                    <ProfileCard/>
-                    <ProfileCard/>
+                    {posts.map(post => <PostCard post={post} key={post.postId}/>)}
                 </Col>
             </Container>
         </>

187be169460e4ca172670ff8b5413330663a5e30
Chris Pittman <cpittman3@cnm.edu>
2021-12-14 14:18:17 -0600

    updated selectpostposttagid

--- backend/src/utils/post/selectPostByPostTagId.ts
+++ backend/src/utils/post/selectPostByPostTagId.ts
@@ -2,13 +2,13 @@ import {connect} from "../database.utils";
 import {Post} from "../interfaces/Post";
 import {RowDataPacket} from 'mysql2';
 
-export async function selectPostByPostTagId(postTagId: string) : Promise<Post | null> {
+export async function selectPostByPostTagId(postTagId: string) : Promise<Post[]> {
     try {
         const mySqlConnection = await connect();
         const mySqlQuery: string = 'SELECT BIN_TO_UUID(postId) AS postId, postProfileId, postContactInfo, postEmail, postDescription, postPosition, postLogo, postCompany, postWebsite FROM post JOIN postTag ON post.postId = postTag.postTagPostId WHERE postTagPostId = UUID_TO_BIN(:postTagPostId)'
         const result = await mySqlConnection.execute(mySqlQuery, {postTagId}) as RowDataPacket[]
         const rows: Post[] = result[0] as Post[]
-        return rows.length === 1 ? {...rows[0]} : null;
+        return rows
     } catch (error) {
         throw error
     }
